dcf7311cf961dcf96ed674008fe60bab045b441d
HAPPY <happy37135535@gmail.com>
2026-01-03 10:24:53 +0700

    Create Combat.lua

---
+++ Combat.lua
@@ -0,0 +1,12 @@
+local ScrollingTab = game.Players.LocalPlayer.PlayerGui:WaitForChild("BloxFruitHubGui"):WaitForChild("Main"):WaitForChild("ScrollingTab")
+
+local Combat = Instance.new("Frame")
+Combat.Name = "Combat"
+Combat.Size = UDim2.new(1, 0, 1, 0)
+Combat.BackgroundColor3 = Color3.new(1, 0, 0)
+Combat.BackgroundTransparency = 1
+Combat.BorderSizePixel = 0
+Combat.BorderColor3 = Color3.new(0, 0, 0)
+Combat.Visible = false
+Combat.Transparency = 1
+Combat.Parent = ScrollingTab
