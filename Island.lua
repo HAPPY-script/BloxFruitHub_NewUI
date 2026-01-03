06d1d2429d6bff7e899e19440ad40ff83501b462
HAPPY <happy37135535@gmail.com>
2026-01-03 09:50:31 +0700

    Create Island.lua

---
+++ Island.lua
@@ -0,0 +1,12 @@
+local ScrollingTab = game.Players.LocalPlayer.PlayerGui:WaitForChild("BloxFruitHubGui"):WaitForChild("Main"):WaitForChild("ScrollingTab")
+
+local Island = Instance.new("Frame")
+Island.Name = "Island"
+Island.Size = UDim2.new(1, 0, 1, 0)
+Island.BackgroundColor3 = Color3.new(0, 0.862745, 1)
+Island.BackgroundTransparency = 1
+Island.BorderSizePixel = 0
+Island.BorderColor3 = Color3.new(0, 0, 0)
+Island.Visible = false
+Island.Transparency = 1
+Island.Parent = ScrollingTab
