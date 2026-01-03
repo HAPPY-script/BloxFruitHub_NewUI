c611988e33a67515dbbc0253c95a2ae37f7e5bb1
HAPPY <happy37135535@gmail.com>
2026-01-03 09:50:01 +0700

    Create Player Setting.lua

---
+++ Player Setting.lua
@@ -0,0 +1,12 @@
+local ScrollingTab = game.Players.LocalPlayer.PlayerGui:WaitForChild("BloxFruitHubGui"):WaitForChild("Main"):WaitForChild("ScrollingTab")
+
+local Player_Setting = Instance.new("Frame")
+Player_Setting.Name = "Player Setting"
+Player_Setting.Size = UDim2.new(1, 0, 1, 0)
+Player_Setting.BackgroundColor3 = Color3.new(0.784314, 0.784314, 1)
+Player_Setting.BackgroundTransparency = 1
+Player_Setting.BorderSizePixel = 0
+Player_Setting.BorderColor3 = Color3.new(0, 0, 0)
+Player_Setting.Visible = false
+Player_Setting.Transparency = 1
+Player_Setting.Parent = ScrollingTab
