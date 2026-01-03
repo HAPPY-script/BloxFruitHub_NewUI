55579f9b326c8f2bd45d82f17520b782db0c93f0
HAPPY <happy37135535@gmail.com>
2026-01-03 09:51:11 +0700

    Create Order.lua

---
+++ Order.lua
@@ -0,0 +1,12 @@
+local ScrollingTab = game.Players.LocalPlayer.PlayerGui:WaitForChild("BloxFruitHubGui"):WaitForChild("Main"):WaitForChild("ScrollingTab")
+
+local Order = Instance.new("Frame")
+Order.Name = "Order"
+Order.Size = UDim2.new(1, 0, 1, 0)
+Order.BackgroundColor3 = Color3.new(1, 0.862745, 0)
+Order.BackgroundTransparency = 1
+Order.BorderSizePixel = 0
+Order.BorderColor3 = Color3.new(0, 0, 0)
+Order.Visible = false
+Order.Transparency = 1
+Order.Parent = ScrollingTab
