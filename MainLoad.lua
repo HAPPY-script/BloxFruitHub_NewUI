b7e13ed5f0e3fa00a33d1d592ab8454d7145899b
HAPPY <happy37135535@gmail.com>
2026-01-04 10:40:08 +0700

    Update MainLoad.lua

--- MainLoad.lua
+++ MainLoad.lua
@@ -1,67 +1,67 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/MainUI.lua"))()
 
-print("Main UI ✅")
+print("Main UI 1/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Status.lua"))()
 
-print("Status ✅")
+print("Status 2/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Main.lua"))()
 
-print("Main ✅")
+print("Main 3/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Raid.lua"))()
 
-print("Raid ✅")
+print("Raid 4/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Fruit.lua"))()
 
-print("Fruit ✅")
+print("Fruit 5/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Visual.lua"))()
 
-print("Visual ✅")
+print("Visual 6/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Player%20Setting.lua"))()
 
-print("Player Setting ✅")
+print("Player Setting 7/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Combat.lua"))()
 
-print("Combat ✅")
+print("Combat 8/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Island.lua"))()
 
-print("Island ✅")
+print("Island 9/10✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/Order.lua"))()
 
-print("Order ✅")
+print("Order 10/10✅")
 
 --=== UI SYSTEM ============================================================================================================================--
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/SYSTEM_UI/SystemUI.lua"))()
 
-print("UI System ✅")
+print("UI System 1/2✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/SYSTEM_UI/ToggleUIEffect.lua"))()
 
-print("Toggle UI Effect ✅")
+print("Toggle UI Effect 2/2✅")
 
 --=== TAB SYSTEM ============================================================================================================================--
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/SYSTEM/Status_System.lua"))()
 
-print("Status tab System ✅")
+print("Status tab System 1/4✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/SYSTEM/Main_System.lua"))()
 
-print("Main tab System ✅")
+print("Main tab System 2/4✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/SYSTEM/Raid_System.lua"))()
 
-print("Raid tab System ✅")
+print("Raid tab System 3/4✅")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HAPPY-script/BloxFruitHub_NewUI/refs/heads/main/SYSTEM/Combat_System.lua"))()
 
-print("Combat tab System ✅")
+print("Combat tab System 4/4✅")
